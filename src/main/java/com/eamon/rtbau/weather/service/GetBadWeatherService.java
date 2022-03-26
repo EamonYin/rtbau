a2fa779bf4f8162bf7d1cd5a3034ee8205addbcc
XiaoMing <xiaominglovejava@163.com>
2022-03-26 12:28:30 +0800

    第一次代码提交

---
+++ src/main/java/com/eamon/rtbau/weather/service/GetBadWeatherService.java
@@ -0,0 +1,11 @@
+package com.eamon.rtbau.weather.service;
+
+/**
+ * @author:Eamon
+ * @create:2022/3/26,12:12
+ * @version:1.0
+ */
+public interface GetBadWeatherService {
+    //获得明天恶劣天气城市
+    String getBadWeatherCities();
+}
---
+++ src/main/java/com/eamon/rtbau/weather/service/impl/GetBadWeatherImpl.java
@@ -0,0 +1,16 @@
+package com.eamon.rtbau.weather.service.impl;
+
+import com.eamon.rtbau.weather.service.GetBadWeatherService;
+
+/**
+ * @author:Eamon
+ * @create:2022/3/26,12:12
+ * @version:1.0
+ */
+public class GetBadWeatherImpl implements GetBadWeatherService {
+
+    @Override
+    public String getBadWeatherCities() {
+        return null;
+    }
+}
