0074f78e059b3d467277d898c0a426ff4410ff99
EamonYin <xiaominglovejava@163.com>
2022-03-28 20:10:34 +0800

    用户关注后，推送一条包含 填写信息的链接

--- src/main/java/com/eamon/rtbau/user/controller/UserController.java
+++ src/main/java/com/eamon/rtbau/user/controller/UserController.java
@@ -3,6 +3,8 @@ package com.eamon.rtbau.user.controller;
 import com.eamon.rtbau.config.DataRepo;
 import com.zjiecode.wxpusher.client.WxPusher;
 import com.zjiecode.wxpusher.client.bean.Message;
+import com.zjiecode.wxpusher.client.bean.MessageResult;
+import com.zjiecode.wxpusher.client.bean.Result;
 import com.zjiecode.wxpusher.client.bean.callback.AppSubscribeBean;
 import com.zjiecode.wxpusher.client.bean.callback.BaseCallBackReq;
 import lombok.extern.slf4j.Slf4j;
@@ -16,6 +18,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alibaba.fastjson.JSONObject;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
 
 
 /**
@@ -33,29 +40,40 @@ public class UserController {
 
     /**
      * wxpusher回调
+     *
      * @param callBackReq
      * @return
      */
     @PostMapping("/callback")
-    public String callback(@RequestBody BaseCallBackReq callBackReq) {
+    public String callback(@RequestBody BaseCallBackReq callBackReq, HttpServletResponse response) throws IOException {
         log.info("收到wxpusher回调:{}", JSONObject.toJSONString(callBackReq));
+
+//        Message message = new Message();
+//        message.setAppToken(appToken);
+//        message.setContentType(Message.CONTENT_TYPE_TEXT);
+//        message.setContent("不加限制的自由是很可怕的，因为很容易让任何人滑向深渊。");
+//        message.setUid("UID_v6EV1w6wfxypQDukydGumHjL9ALz");
+//        message.setUrl("http://49.233.73.127:1263/#/home");//可选参数
+//        Result<List<MessageResult>> result = WxPusher.send(message);
+//        log.info("消息发送："+result);
+
         if (BaseCallBackReq.ACTION_APP_SUBSCRIBE.equalsIgnoreCase(callBackReq.getAction())) {
             AppSubscribeBean appSubscribeBean = JSONObject.parseObject(JSONObject.toJSONString(callBackReq.getData()), AppSubscribeBean.class);
-            if (!StringUtils.isEmpty(appSubscribeBean.getExtra())) {
-
-                /**
-                 * 写业务
-                 */
+            log.info("【appSubscribeBean】" + appSubscribeBean);
+            if (!StringUtils.isEmpty(appSubscribeBean.getUid())) {
 
                 DataRepo.put(appSubscribeBean.getExtra(), appSubscribeBean.getUid());
                 log.info("存储回调数据:{}", JSONObject.toJSONString(appSubscribeBean));
                 //扫码以后，发送一条消息给用户
                 Message message = new Message();
-                message.setContent("扫描成功，你可以使用demo演示程序发送消息");
-                message.setContentType(Message.CONTENT_TYPE_TEXT);
-                message.setUid(appSubscribeBean.getUid());
                 message.setAppToken(appToken);
-                WxPusher.send(message);
+                message.setContentType(Message.CONTENT_TYPE_TEXT);
+                message.setContent("不加限制的自由是很可怕的，因为很容易让任何人滑向深渊。");
+                message.setUid(appSubscribeBean.getUid());
+                message.setUrl("http://www.xiaoming100.club:1264/#/home");//可选参数
+                Result<List<MessageResult>> result = WxPusher.send(message);
+                log.info("消息发送：" + result);
+                
             } else {
                 //无参数二维码（默认二维码）
             }
