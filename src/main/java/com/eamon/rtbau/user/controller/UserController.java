358b4ee3693fd769f818a242c123b59fd7c06560
EamonYin <xiaominglovejava@163.com>
2022-04-03 17:01:13 +0800

    给前端传uid

--- src/main/java/com/eamon/rtbau/user/controller/UserController.java
+++ src/main/java/com/eamon/rtbau/user/controller/UserController.java
@@ -48,15 +48,6 @@ public class UserController {
     public String callback(@RequestBody BaseCallBackReq callBackReq, HttpServletResponse response) throws IOException {
         log.info("收到wxpusher回调:{}", JSONObject.toJSONString(callBackReq));
 
-//        Message message = new Message();
-//        message.setAppToken(appToken);
-//        message.setContentType(Message.CONTENT_TYPE_TEXT);
-//        message.setContent("不加限制的自由是很可怕的，因为很容易让任何人滑向深渊。");
-//        message.setUid("UID_v6EV1w6wfxypQDukydGumHjL9ALz");
-//        message.setUrl("http://49.233.73.127:1263/#/home");//可选参数
-//        Result<List<MessageResult>> result = WxPusher.send(message);
-//        log.info("消息发送："+result);
-
         if (BaseCallBackReq.ACTION_APP_SUBSCRIBE.equalsIgnoreCase(callBackReq.getAction())) {
             AppSubscribeBean appSubscribeBean = JSONObject.parseObject(JSONObject.toJSONString(callBackReq.getData()), AppSubscribeBean.class);
             log.info("【appSubscribeBean】" + appSubscribeBean);
@@ -70,10 +61,10 @@ public class UserController {
                 message.setContentType(Message.CONTENT_TYPE_TEXT);
                 message.setContent("不加限制的自由是很可怕的，因为很容易让任何人滑向深渊。");
                 message.setUid(appSubscribeBean.getUid());
-                message.setUrl("http://www.xiaoming100.club:1264/#/home");//可选参数
+                message.setUrl("http://www.xiaoming100.club:1263/#/home?uid="+appSubscribeBean.getUid());//可选参数
                 Result<List<MessageResult>> result = WxPusher.send(message);
                 log.info("消息发送：" + result);
-                
+
             } else {
                 //无参数二维码（默认二维码）
             }
@@ -87,5 +78,4 @@ public class UserController {
     public void getWxCallback(String str) {
         System.out.println(str);
     }
-
 }
